49a080c5e7a0df5f0f2ec631d0ae20008287268b
alex <al3xback@gmail.com>
2023-07-11 16:35:56 +0700

    first commit

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,29 @@
+const Footer = () => {
+	return (
+		<footer>
+			<div className="container">
+				<p>
+					Challenge by&nbsp;
+					<a
+						href="https://www.frontendmentor.io?ref=challenge"
+						className="btn btn--link"
+						rel="noopener noreferrer"
+						target="_blank">
+						Frontend Mentor
+					</a>
+					. Coded by&nbsp;
+					<a
+						href="https://github.com/al3xback"
+						className="btn btn--link"
+						rel="noopener noreferrer"
+						target="_blank">
+						al3xback
+					</a>
+					.
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ src/components/Layout/Main.js
@@ -0,0 +1,44 @@
+import meetingImage from '../../assets/images/meeting.jpg';
+
+const Main = () => {
+	return (
+		<main>
+			<div className="container">
+				<article className="card">
+					<div className="card__image">
+						<div className="card__image-inner">
+							<img src={meetingImage} alt="" />
+						</div>
+					</div>
+					<div className="card__content">
+						<h2 className="card__title">
+							Get <mark>insights</mark> that help your business
+							grow.
+						</h2>
+						<p className="card__desc">
+							Discover the benefits of data analytics and make
+							better decisions regarding revenue, customer
+							experience, and overall efficiency.
+						</p>
+						<ul className="card__stats-list">
+							<li className="card__stats-list-item">
+								<span className="num">10K+</span>
+								<span className="label">Companies</span>
+							</li>
+							<li className="card__stats-list-item">
+								<span className="num">314</span>
+								<span className="label">Templates</span>
+							</li>
+							<li className="card__stats-list-item">
+								<span className="num">12M+</span>
+								<span className="label">Queries</span>
+							</li>
+						</ul>
+					</div>
+				</article>
+			</div>
+		</main>
+	);
+};
+
+export default Main;
