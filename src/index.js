d9dd44af1b6e225d2351db9398438307123aca27
alex <al3xback@gmail.com>
2023-07-12 18:16:45 +0700

    splitted css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './assets/css/main.css';
+import './assets/scss/main.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
